dcd571e36e920d7a5ca54246fd2f852d746bfc2e
anbarasupr <anbarasu.2013@gmail.com>
2020-01-29 23:02:06 +0530

    upd

---
+++ java8/part2/src/org/firstlamda/RunnableLambda.java
@@ -0,0 +1,34 @@
+package org.firstlamda;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.util.Iterator;
+
+public class RunnableLambda {
+
+	public static void main(String[] args) throws InterruptedException {
+		// anonymous class
+		Runnable runnable = new Runnable() {
+			@Override
+			public void run() {
+				for (int i = 0; i < 3; i++) {
+					System.out.println("Hello from thread [" + Thread.currentThread().getName() + "]");
+				}
+			}
+		};
+
+		// lambda exp
+		Runnable runnableLambda= ()->{
+			for (int i = 0; i < 3; i++) {
+				System.out.println("Hello Lambda from thread [" + Thread.currentThread().getName() + "]");
+			}
+		};
+		
+		Thread t1=new Thread(runnable,"anonymous thread");
+		t1.start();
+		
+		Thread t2=new Thread(runnable,"lambda thread");
+		t2.start();
+	}
+
+}
